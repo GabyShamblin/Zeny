bffc54267565d3651c6e5b94e56fa23a5f4b73c1
Julia Oghigian <jrojro12@gmail.com>
2022-07-31 13:32:18 -0700

    First draft

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module Zeny
 go 1.18
 
 require (
+	github.com/gomodule/redigo v1.8.9 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0
 	github.com/joho/godotenv v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,24 +1,88 @@
 package main
 
 import (
-	//"fmt"
-	//"github.com/gorilla/mux"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
 
+type ChatMessage struct {
+	Text string `json:"text"`
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcaster = make(chan ChatMessage)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// ensure connection close when function returns
+	defer ws.Close()
+	clients[ws] = true
+
+	for {
+		var msg ChatMessage
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+		// send new message to the channel
+		broadcaster <- msg
+	}
+}
+func handleMessages() {
+	for {
+		// grab any next message from channel
+		msg := <-broadcaster
+
+		messageClients(msg)
+	}
+}
+func messageClients(msg ChatMessage) {
+	// send to every client currently connected
+	for client := range clients {
+		messageClient(client, msg)
+	}
+}
+func messageClient(client *websocket.Conn, msg ChatMessage) {
+
+	err := client.WriteJSON(msg)
+	if err != nil {
+		log.Printf("error: %v", err)
+		client.Close()
+		delete(clients, client)
+	}
+}
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	port := "8080"
+
+	if err != nil {
+		panic(err)
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Print("Server starting at localhost:8080")
+	http.HandleFunc("/websocket", handleConnections)
+	go handleMessages()
+
+	log.Print("Server starting at localhost:" + port)
+
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
-
 }
